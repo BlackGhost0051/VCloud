58ead77797c21cda25b820610236028da97fbf3d
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-11-15 11:54:50 +0100

    Add sqlite support

--- src/main/java/com/example/VCloud/Managers/DataBaseManager.java
+++ src/main/java/com/example/VCloud/Managers/DataBaseManager.java
@@ -2,14 +2,29 @@ package com.example.VCloud.Managers;
 
 public class DataBaseManager {
 
-    /*
+    private String dataBaseStructure =
+            "CREATE TABLE users (\n" +
+            "        id INTEGER PRIMARY KEY,\n" +
+            "        email TEXT NOT NULL,\n" +
+            "        login TEXT NOT NULL UNIQUE,\n" +
+            "        password TEXT NOT NULL\n" +
+            "    );";
 
-    CREATE TABLE users (
-        id INTEGER PRIMARY KEY,
-        mail TEXT NOT NULL,
-        login TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-    );
+    private String dataBaseName = "database.db";
 
-    */
+
+    public DataBaseManager() {
+        initDataBase();
+    }
+
+
+    public boolean registerUser(String login, String email,String password) {
+
+
+        return false;
+    }
+
+    private void initDataBase(){
+
+    }
 }
