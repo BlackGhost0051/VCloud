eb7c70e682e814f30e7eb6443715bafb3c4a8088
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-11-18 16:58:40 +0100

    Add test login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -11,5 +11,16 @@ login_button.addEventListener("click", function (){
     let login = login_input.value;
     let password = pass_input.value;
 
-    console.log(login + " " + password);
+    fetch("/login?login=" + login + "&password=" + password,{
+        method: "POST",
+    }).then(responce => {
+        if(!responce.ok){
+            throw new Error("Network response was not ok " + response.statusText)
+        }
+        return responce.text();
+    }).then(data => {
+        alert(data);
+    }).catch(error => {
+        alert(error.message);
+    });
 });
