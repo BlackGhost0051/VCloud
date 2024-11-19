f03c79ff91b9a03deab68d6331a25de60f287239
Black Ghost <147267828+BlackGhost0051@users.noreply.github.com>
2024-11-19 12:19:31 +0100

    Add structure

--- src/main/java/com/example/VCloud/Managers/JWTManager.java
+++ src/main/java/com/example/VCloud/Managers/JWTManager.java
@@ -1,4 +1,25 @@
 package com.example.VCloud.Managers;
 
 public class JWTManager {
+    private static final String SECRET_KEY = "test-secret-key";  // make hash ?
+
+    public JWTManager() {
+
+    }
+
+    public String generateToken(String username) {
+        return "";
+    }
+
+    public boolean verifyToken(String token) {
+        return false;
+    }
+
+    public String getUsername(String token) {
+        return "";
+    }
+
+    public String getEmail(String token) {
+        return "";
+    }
 }
