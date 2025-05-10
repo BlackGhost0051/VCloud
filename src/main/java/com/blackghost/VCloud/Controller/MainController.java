c78c9b4b0da324f418a50b38ff5e7429dc47b190
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-05-10 17:58:23 +0200

    Add connect to database

--- src/main/java/com/blackghost/VCloud/Controller/MainController.java
+++ src/main/java/com/blackghost/VCloud/Controller/MainController.java
@@ -1,9 +1,19 @@
 package com.blackghost.VCloud.Controller;
 
+import com.blackghost.VCloud.Service.DataBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MainController {
 
+    private final DataBaseService dataBaseService;
+
+    @Autowired
+    public MainController(DataBaseService dataBaseService) {
+        this.dataBaseService = dataBaseService;
+    }
 
     @GetMapping("/")
     public String index(){
--- src/main/java/com/blackghost/VCloud/Service/DataBaseService.java
+++ src/main/java/com/blackghost/VCloud/Service/DataBaseService.java
@@ -1,6 +1,32 @@
 package com.blackghost.VCloud.Service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.sql.*;
+
+@Service
 public class DataBaseService {
+
+    private final Connection connection;
+
+    public DataBaseService(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String user,
+            @Value("${spring.datasource.password}") String pass
+    ) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connected.");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to connect", e);
+        }
+    }
+
+    void init(){
+
+    }
 
     void addUser(){}
 
